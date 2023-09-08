b06ac409af8951c733e721f34d145597ab7fd56f
paulabeatrizsoliveira <biamillez@gmail.com>
2023-09-08 16:17:57 -0300

    Criei os atributos e o construtor.

--- src/Pessoa/Cliente.java
+++ src/Pessoa/Cliente.java
@@ -2,4 +2,13 @@ package cliente;
 
 public class Cliente {
 
+	
+	private String cpf;
+	private String senha;
+	
+	public Cliente (String cpf, String senha){
+		this.cpf = cpf;
+		this.senha= senha;
+	}
+	
 }
--- src/Pessoa/Pessoa.java
+++ src/Pessoa/Pessoa.java
@@ -2,4 +2,9 @@ package cliente;
 
 public class Pessoa{
 	
+	private String cpf;
+	private String senha;
+	
+	
+	
 }
