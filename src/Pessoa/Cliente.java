3a015adfe3fba78d7e94eb88f9cb8433ec65b8dd
paulabeatrizsoliveira <biamillez@gmail.com>
2023-09-08 16:39:28 -0300

    Construção Cliente;

--- src/Pessoa/Cliente.java
+++ src/Pessoa/Cliente.java
@@ -1,14 +1,34 @@
 package cliente;
 
 public class Cliente {
-
-	
+	//Atributos
 	private String cpf;
 	private String senha;
 	
-	public Cliente (String cpf, String senha){
+	//Construtor
+	public Cliente(String cpf, String senha) {
 		this.cpf = cpf;
-		this.senha= senha;
+		this.senha = senha;
+	}
+	//Get and set
+	public String getCpf() {
+		return cpf;
+
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+
+	}
+
+	public void setCpf (String cpf) {
+		this.cpf = cpf;
 	}
 	
-}
+	public void setSenha (String senha) {
+		this.senha = senha;
+		
+	}
+		
+	}
+
