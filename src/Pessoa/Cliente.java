bcbc01722faf86cfe5ab1e3022157d324417bd72
paulabeatrizsoliveira <biamillez@gmail.com>
2023-09-09 09:53:57 -0300

    Criei a classe cliente e pessoa, adicionei get and sett, construtores;

--- src/Pessoa/Cliente.java
+++ src/Pessoa/Cliente.java
@@ -1,34 +1,19 @@
-package cliente;
+package pessoa;
 
-public class Cliente {
-	//Atributos
-	private String cpf;
-	private String senha;
+public class Cliente extends Pessoa {
+
+	// Construtor
+
+	public Cliente() {
+		super();
+	}
+
+	public Cliente(String usuario, String senha, String email, String telefone, String cpf) {
+		super(usuario, senha, email, telefone, cpf);
+	}
+
 	
-	//Construtor
-	public Cliente(String cpf, String senha) {
-		this.cpf = cpf;
-		this.senha = senha;
-	}
-	//Get and set
-	public String getCpf() {
-		return cpf;
 
 	}
 
-	public void setSenha(String senha) {
-		this.senha = senha;
-
-	}
-
-	public void setCpf (String cpf) {
-		this.cpf = cpf;
-	}
-	
-	public void setSenha (String senha) {
-		this.senha = senha;
-		
-	}
-		
-	}
 
