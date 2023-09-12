f26a34b63ac5ee45cde7977befaa5087a26ad265
CarlosAlbertoMPZ <carlosalbertompz@gmail.com>
2023-09-12 14:55:31 -0300

    Adicionei o metodo que filtra a quantidade de contas iguas ao tipo de agencia.

--- src/sistemaInterno/App.java
+++ src/sistemaInterno/App.java
@@ -27,10 +27,13 @@
 	
 package sistemaInterno;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import agencia.ETipoAgencia;
 import cliente.Cliente;
+import conta.Conta;
 import funcionario.Funcionario;
 import funcionario.Gerente;
 import pessoa.ETipoAcesso;
@@ -46,7 +49,16 @@ public class App {
 		
 		
 	}
-		
+	
+	/*public static List<Conta> totalContas (List<Conta> contas, ETipoAgencia tipoAgencia){
+		List<Conta> listaContas = new ArrayList<Conta>();
+		for ( int i=0; i<contas.size(); i++) {
+			if (contas.get(i).getIdAgencia().getIdAgencia() == tipoAgencia.getIdAgencia() ) {
+				listaContas.add(contas.get(i));
+			}
+		}
+		return listaContas;*/
+	
 	public static void logar(Pessoa pessoa) {		
 		
 		Scanner leitor = new Scanner(System.in);
